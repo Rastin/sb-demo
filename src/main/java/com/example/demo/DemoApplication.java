ada9935bae3d56db8344b4d601617c7e377ae8b2
Rastin <rastin.ken@gmail.com>
2021-01-26 07:18:56 -0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	@RequestMapping("/")
+	String home() {
+	return "OpenShift Spring Boot DEV v1.0";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
